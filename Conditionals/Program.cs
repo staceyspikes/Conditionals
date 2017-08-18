c4bd6f748bf61223f86065f56dd2936cd516e16b
staceyspikesgit config --global user.name staceyspikes <stacey.lynne13@yahoo.com>
2017-08-17 20:52:03 -0400

    added

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,17 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            int age = 21;
+
+            if (age >= 17)
+            {
+                Console.WriteLine("You can see this movie");
+
+            }
+            else
+            {
+                Console.WriteLine("Sorry you are too young you need a parent present");
+            }
         }
     }
 }
